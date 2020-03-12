acca47dadd35ae94ee08b0edde413f1116b8e869
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2020-03-12 06:30:37 -0500

    [app1] commit 2: linking each card to a user id. Creating a new location

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Grid, Segment, Feed, Icon } from 'semantic-ui-react';
 import './UserItem.css';
 
 const UserItem = (props) => {
     return (
         <Grid.Column>
-            <Segment>
-                <Feed size='large'>
-                    <Feed.Event>
-                        <Feed.Label image={props.image} />
-                        <Feed.Content>
-                            <Feed.Summary>
-                                <Feed.User>{props.name}</Feed.User>
-                                <Feed.Date>1 Hour Ago</Feed.Date>
-                            </Feed.Summary>
+            <Link to={`/${props.id}/places`}>
+                <Segment>
+                    <Feed size='large'>
+                        <Feed.Event>
+                            <Feed.Label image={props.image} />
+                            <Feed.Content>
+                                <Feed.Summary>
+                                    <span className="feed-user">{props.name}</span>
+                                    <Feed.Date>1 Hour Ago</Feed.Date>
+                                </Feed.Summary>
 
-                            <Feed.Meta>
-                                <Feed.Like>
-                                    <Icon name='like' />4 Likes
-                                </Feed.Like>
-                            </Feed.Meta>
-                        </Feed.Content>
-                    </Feed.Event>
-                </Feed>
-            </Segment>
+                                <Feed.Meta>
+                                    <span className="feed-like">
+                                        <Icon name='map marker alternate' />4 places
+                                    </span>
+                                </Feed.Meta>
+                            </Feed.Content>
+                        </Feed.Event>
+                    </Feed>
+                </Segment>
+            </Link>
         </Grid.Column>
     );
 }
